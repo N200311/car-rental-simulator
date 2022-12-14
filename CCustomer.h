9ac148c3c9760c805162fab736cc6111694d1a62
N200311 <110634551+N200311@users.noreply.github.com>
2022-12-14 14:14:15 +0200

    Add files via upload

---
+++ CCustomer.h
@@ -0,0 +1,99 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+class CCustomer {
+private:
+	//variables
+	int customer_id;
+	long long int customer_num;
+	long long int emergency_contact;
+	string customer_name;
+	string customer_email;
+	string customer_password;
+public:
+	//constructor
+	CCustomer() {
+		customer_id = 0;
+		customer_num = 0;
+		emergency_contact = 0;
+		customer_name.clear();
+		customer_email.clear();
+		customer_password.clear();
+	}
+	
+	//setters
+	void set_id(int id) {
+		customer_id = id;
+	}
+	void set_num(long long int num) {
+		customer_num = num;
+	}
+	void set_emergency(long long int emergency) {
+		emergency_contact = emergency;
+	}
+	void set_name(string name) {
+		customer_name = name;
+	}
+	void set_email(string email) {
+		customer_email = email;
+	}
+	void set_password(string password) {
+		customer_password = password;
+	}
+	
+	//getters
+	int get_id() {
+		return customer_id;
+	}
+	long long int get_num() {
+		return customer_num;
+	}
+	
+	long long int get_emergency() {
+		return emergency_contact;
+	}
+	string get_name() {
+		return customer_name;
+	}
+	string get_email() {
+		return customer_email;
+	}
+	string get_password() {
+		return customer_password;
+	}
+
+	//functions
+	void add_customer_info(CCustomer customer) {
+		cout<<"\nPlease enter customers information:"<<endl;
+		cout << "enter customer's name: ";//1
+		cin >> customer.customer_name;
+		set_name(customer.customer_name);
+		cout << "enter customer's email: ";//2
+		cin >> customer.customer_email;
+		set_email(customer.customer_email);
+		cout << "enter customer's number: ";//3
+		cin >> customer.customer_num;
+		set_num(customer.customer_num);
+		cout << "enter customer's emergency contact number: ";//4
+		cin >> customer.emergency_contact;
+		set_emergency(customer.emergency_contact);
+		cout << "enter customer's id: ";
+		cin >> customer.customer_id;
+		set_id(customer.customer_id);
+		cout << "enter customer's password: ";
+		cin >> customer.customer_password;
+		set_password(customer.customer_password);
+	}
+	void get_customer_info(CCustomer customer)
+	{
+		cout << endl;
+		cout << "name:                  " << customer.get_name() << endl;
+		cout << "email:                 " << customer.get_email() << endl;
+		cout << "phone number:          " << customer.get_num() << endl;
+		cout << "emergency contact:     " << customer.get_emergency() << endl;
+		cout << "ID:                    " << customer.get_id() << endl;
+		cout << "password:              " << customer.get_password() << endl;
+
+	}
+};
